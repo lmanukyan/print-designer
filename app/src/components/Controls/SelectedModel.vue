53e636cefb62b7c74d1c1531dea91da7cdfdce81
lmanukyan <levon.manukyan97@gmail.com>
2022-12-08 18:32:06 +0400

    Селектор цветов

--- app/src/components/Controls/SelectedModel.vue
+++ app/src/components/Controls/SelectedModel.vue
@@ -1,13 +1,12 @@
 <template>
-
   <div class="q-pb-md q-gutter-sm">
-    <q-btn 
+    <q-btn
       @click="setCatalogModalOpened(true)"
       no-caps
       color="white"
       icon="shopping_bag"
       text-color="black"
-      label="каталог" 
+      label="каталог"
     />
     <q-btn
       @click="makeCustomModel()"
@@ -19,34 +18,37 @@
   </div>
 
   <q-card class="my-card" v-if="selectedModel">
-
     <q-card-section>
-        <div class="text-h6">{{ selectedModel.title }}</div>
+      <div class="text-h6">{{ selectedModel.title }}</div>
     </q-card-section>
 
-    <q-img :src="selectedModel.images[0].front.url" /> 
+    <q-img :src="selectedModel.images[0].front.url" />
 
     <q-card-section>
       <p class="text-subtitle1">Цвет</p>
       <q-card-actions>
         <div
-          v-for="image in selectedModel.images" 
+          v-for="image in selectedModel.images"
           :key="image.front.url"
           @click="setSelectedModelColor(image)"
           class="cursor-pointer model-colors"
           :class="{ 'selected-model': selectedModelColor.id == image.id }"
         >
-          <img :src="image.front.url">
+          <q-icon
+            v-if="selectedModelColor.id == image.id"
+            name="check"
+            size="34px"
+          />
+          <img :src="image.front.url" />
         </div>
       </q-card-actions>
-
 
       <q-item tag="label">
         <q-item-section>
           <q-item-label>Цена</q-item-label>
         </q-item-section>
         <q-item-section side>
-          <span class="text-bold">{{price}}</span>
+          <span class="text-bold">{{ price }} ₽</span>
         </q-item-section>
       </q-item>
 
@@ -72,14 +74,16 @@
         <q-list separator dense class="q-pa-md">
           <q-item class="text-bold no-padding">
             <q-item-section>Размер</q-item-section>
-            <q-item-section class="q-mr-md text-right">Kоличество</q-item-section>
+            <q-item-section class="q-mr-md text-right"
+              >Kоличество</q-item-section
+            >
           </q-item>
           <template v-for="size in selectedModel.sizes" :key="size">
             <q-item class="no-padding">
               <q-item-section v-text="size"></q-item-section>
               <q-item-section avatar>
-                <q-input 
-                  v-model="sizes[size]" 
+                <q-input
+                  v-model="sizes[size]"
                   type="number"
                   input-class="text-right"
                   min="0"
@@ -91,7 +95,9 @@
           </template>
           <q-item class="text-bold no-padding">
             <q-item-section>Общее</q-item-section>
-            <q-item-section class="q-mr-md text-right">{{ quantity }}</q-item-section>
+            <q-item-section class="q-mr-md text-right">{{
+              quantity
+            }}</q-item-section>
           </q-item>
         </q-list>
       </template>
@@ -100,17 +106,13 @@
         <q-btn
           no-caps
           color="primary"
-          icon="shopping_basket" 
+          icon="shopping_basket"
           label="Заказать"
-          @click="orderModal=true"
+          @click="orderModal = true"
           :disabled="!canOrder()"
         />
       </div>
-
     </q-card-section>
-
-    
-
 
     <q-dialog v-model="orderModal">
       <q-card style="min-width: 350px">
@@ -120,12 +122,12 @@
         <q-card-section class="q-pt-none">
           <q-input
             v-model="phone"
-            mask="+7 (###) ###-##-##" 
+            mask="+7 (###) ###-##-##"
             ref="phoneInput"
             fill-mask
             filled
             lazy-rules
-            :rules="[ val => isValidPhone(val) || 'заполните номер телефона']"
+            :rules="[(val) => isValidPhone(val) || 'заполните номер телефона']"
           />
         </q-card-section>
         <q-card-actions align="right">
@@ -139,124 +141,101 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
-    
-    
-
-
   </q-card>
-
-  
-
-
-
-
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import { mapActions } from 'vuex'
-import { mapGetters } from 'vuex'
-import { mapMutations } from 'vuex'
-import { ComputedMutator } from '@/utils'
+import { mapState } from "vuex";
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+import { mapMutations } from "vuex";
+import { ComputedMutator } from "@/utils";
 
-import OrderService from '@/services/order'
-import CanvasService from '@/services/canvas'
+import OrderService from "@/services/order";
+import CanvasService from "@/services/canvas";
 
 export default {
-  name: 'SelectedModel',
+  name: "SelectedModel",
 
   data: () => ({
     qualityOptions: [
-      {label: 'тонкий', value: 'thin'},
-      {label: 'плотный', value: 'dense'}
+      { label: "тонкий", value: "thin" },
+      { label: "плотный", value: "dense" },
     ],
     orderModal: false,
     loading: false,
-    phone: ''
+    phone: "",
   }),
 
   watch: {
     sizes: {
-      handler(){
-        this.calculatePrice()
+      handler() {
+        this.calculatePrice();
       },
-      deep: true
+      deep: true,
     },
   },
 
   computed: {
-    ...mapState('app', [
-      'user',
-      'pricing',
-    ]),
-    ...mapState('order', [
-      'price',
-      'sizes',
-    ]),
-    ...mapState('product', [
-      'selectedModel',
-      'selectedModelColor',
-    ]),
-    ...mapState('canvas', [
-      'layers',
-    ]),
-    ...mapGetters('order', ['quantity']),
-    
-    urgency: ComputedMutator('order', 'urgency', 'setUrgency'),
-    quality: ComputedMutator('order', 'quality', 'setQuality'),
+    ...mapState("app", ["user", "pricing"]),
+    ...mapState("order", ["price", "sizes"]),
+    ...mapState("product", ["selectedModel", "selectedModelColor"]),
+    ...mapState("canvas", ["layers"]),
+    ...mapGetters("order", ["quantity"]),
+
+    urgency: ComputedMutator("order", "urgency", "setUrgency"),
+    quality: ComputedMutator("order", "quality", "setQuality"),
   },
 
   methods: {
-    ...mapMutations('product', [
-      'setSelectedModelColor',
+    ...mapMutations("product", ["setSelectedModelColor"]),
+    ...mapMutations("app", [
+      "setCatalogModalOpened",
+      "setAuthModalOpened",
+      "setCustomProductModalOpened",
     ]),
-    ...mapMutations('app', [
-      'setCatalogModalOpened',
-      'setAuthModalOpened',
-      'setCustomProductModalOpened',
-    ]),
-    ...mapActions('order', ['calculatePrice']),
+    ...mapActions("order", ["calculatePrice"]),
 
-    isValidPhone(value){
-      return value.match(/[0-9]/g)?.length == 11
+    isValidPhone(value) {
+      return value.match(/[0-9]/g)?.length == 11;
     },
 
-    canOrder(){
+    canOrder() {
       return (
         (!this.selectedModel.clientModel && this.quantity > 0) ||
         (this.selectedModel.clientModel && this.layers.length > 0)
-      )
+      );
     },
 
-    quantityHtml(){
-      let html = '';
-      for(let size in this.sizes){
-        if(this.sizes[size] == 0) continue;
-        html +=  `<b>${size.toUpperCase()}:</b> ${this.sizes[size]}<br/>`
+    quantityHtml() {
+      let html = "";
+      for (let size in this.sizes) {
+        if (this.sizes[size] == 0) continue;
+        html += `<b>${size.toUpperCase()}:</b> ${this.sizes[size]}<br/>`;
       }
-      return html
+      return html;
     },
 
-    async makeCustomModel(){
-      if(this.user){
+    async makeCustomModel() {
+      if (this.user) {
         this.setCustomProductModalOpened(true);
-      }else {
+      } else {
         this.setAuthModalOpened(true);
       }
     },
 
-    async createOrder(){
-      if(!this.isValidPhone(this.phone)){
+    async createOrder() {
+      if (!this.isValidPhone(this.phone)) {
         this.$refs.phoneInput.validate();
         return;
       }
 
-      this.loading = true
-      
+      this.loading = true;
+
       let captures = await CanvasService.capture();
 
       let order = await OrderService.create({
-        title: 'Order №' + Date.now(),
+        title: "Order №" + Date.now(),
         clientModel: this.selectedModel.clientModel,
         phone: this.phone,
         price: this.price,
@@ -268,25 +247,23 @@ export default {
         json: JSON.stringify({
           sizes: this.sizes,
           model: this.selectedModelColor,
-          canvasData: CanvasService.toJSON()
-        })
+          canvasData: CanvasService.toJSON(),
+        }),
       });
 
-      if(order){
-        this.orderModal = false
-        this.phone = ''
+      if (order) {
+        this.orderModal = false;
+        this.phone = "";
       }
-      this.loading = false
+      this.loading = false;
     },
-
-    
   },
-
-}
+};
 </script>
 
 <style lang="scss">
 .model-colors {
+  position: relative;
   width: 45px;
   height: 45px;
   display: flex;
@@ -296,7 +273,7 @@ export default {
   overflow: hidden;
   margin-right: 5px;
   margin-bottom: 5px;
-  border: 4px solid transparent;
+  border: 1px solid #ccc;
 
   img {
     width: 110%;
@@ -305,7 +282,19 @@ export default {
   }
 
   &.selected-model {
-    border-color: $primary;
+    i {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #000;
+      font-weight: bold;
+      background: #ffffff82;
+    }
   }
 }
 </style>

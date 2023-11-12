b772501aa62eae65220b4f0be79b9b43efd32843
lmanukyan <levon.manukyan97@gmail.com>
2023-11-12 21:04:25 +0400

    dist build

--- src/collections/Orders.ts
+++ src/collections/Orders.ts
@@ -1,14 +1,14 @@
-import nunjucks from 'nunjucks';
-import OrderCreated from '../hooks/order-created'
-import { CollectionConfig } from 'payload/types'
-import { OwnerAccess } from '../access'
-import { AuthorField } from '../shared/fields'
+import nunjucks from "nunjucks";
+import OrderCreated from "../hooks/order-created";
+import { CollectionConfig } from "payload/types";
+import { OwnerAccess } from "../access";
+import { AuthorField } from "../shared/fields";
 
 const Orders: CollectionConfig = {
-  slug: 'orders',
+  slug: "orders",
   admin: {
-    defaultColumns: ['title', 'phone', 'price', 'createdAt'],
-    useAsTitle: 'title',
+    defaultColumns: ["title", "phone", "price", "createdAt"],
+    useAsTitle: "title",
     disableDuplicate: true,
   },
   access: {
@@ -18,97 +18,95 @@ const Orders: CollectionConfig = {
     delete: OwnerAccess,
   },
   hooks: {
-    afterChange: [
-      OrderCreated
-    ]
+    afterChange: [OrderCreated],
   },
   fields: [
     {
-      name: 'title',
-      type: 'text',
+      name: "title",
+      type: "text",
       required: true,
       admin: { readOnly: true },
     },
     {
-      name: 'clientModel',
-      type: 'checkbox',
+      name: "clientModel",
+      type: "checkbox",
       required: true,
       admin: { readOnly: true },
     },
     {
-      name: 'phone',
-      type: 'text',
+      name: "phone",
+      type: "text",
       required: true,
       admin: { readOnly: true },
     },
     {
-      name: 'email',
-      type: 'text',
+      name: "email",
+      type: "text",
       required: true,
       admin: { readOnly: true },
     },
     {
-      name: 'price',
-      type: 'text',
+      name: "price",
+      type: "text",
       required: true,
       admin: { readOnly: true },
       validate: () => true, // allow number value
     },
     {
-      name: 'quantity',
-      type: 'code',
-      admin: { 
-        readOnly: true, 
-        language: 'html',
-        condition: (data) => ! data.clientModel
+      name: "quantity",
+      type: "code",
+      admin: {
+        readOnly: true,
+        language: "html",
+        condition: (data) => !data.clientModel,
       },
     },
     {
-      name: 'front',
-      type: 'upload',
-      relationTo: 'media',
+      name: "front",
+      type: "upload",
+      relationTo: "media",
       required: true,
       admin: { hidden: true },
     },
     {
-      name: 'back',
-      type: 'upload',
-      relationTo: 'media',
+      name: "back",
+      type: "upload",
+      relationTo: "media",
       admin: { hidden: true },
     },
     {
-      name: 'json',
-      type: 'code',
+      name: "json",
+      type: "code",
       admin: {
-        language: 'json',
+        language: "json",
         readOnly: true,
-      }
+      },
     },
     AuthorField,
   ],
 
   endpoints: [
     {
-      path: '/:id/print',
-      method: 'get',
+      path: "/:id/print",
+      method: "get",
       handler: async (req, res, next) => {
-        try {        
+        try {
           const order = await req.payload.findByID({
-            collection: 'orders', 
-            id: req.params.id
+            collection: "orders",
+            id: req.params.id,
           });
 
           const html = nunjucks.render(`order-created.njk`, {
             ...order,
-            json: JSON.parse(order.json)
+            json: JSON.parse(order.json as string),
           });
           res.send(html);
-        } catch(e) {
+        } catch (e) {
           res.send(e);
         }
-      }
-    }
-  ]
-}
+      },
+    },
+  ],
+};
 
 export default Orders;
